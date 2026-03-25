db11e1086c42f7ee64a1b3dd8a5f43e081c61927
MK <ygmcafee59@gmail.com>
2026-03-25 23:25:58 +0900

    update app

--- app.js
+++ app.js
@@ -416,6 +416,11 @@ function getCueTextUntilBoundary(startIndex) {
       continue;
     }
 
+    if (index === startIndex) {
+      parts.push(text);
+      continue;
+    }
+
     const boundaryIndex = text.search(boundaryRe);
     if (boundaryIndex >= 0) {
       parts.push(text.slice(0, boundaryIndex + 1).trim());
